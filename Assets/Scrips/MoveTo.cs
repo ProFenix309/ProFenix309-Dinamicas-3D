a347a59cb2503de7dc2498ac32ad9b8daa148e01
SrSwagsito <roerwy1809@gmail.com>
2025-03-14 17:54:37 -0500

    14/03/25

--- Assets/Scrips/MoveTo.cs
+++ Assets/Scrips/MoveTo.cs
@@ -10,17 +10,49 @@ public class MoveTo : MonoBehaviour
 
     [SerializeField] Animator animator;
 
-    // Start is called before the first frame update
+    [SerializeField] float tiempoDeEspera;
+    float tiempoInicio;
+    bool moverse = false;
+
+    [SerializeField] float actionDistans;
+    [SerializeField] float distanciaActual;
+
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        tiempoInicio = Time.time;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(targetAgent.position);
+        //Temporizador para activar el movimiento
+        if (!moverse && Time.time - tiempoInicio >= tiempoDeEspera)
+        {
+            moverse = true;
+        }
 
+        if (moverse)
+        {
+            MoverNPC();
+            EjecutarAnimaciones();
+        }
+    }
+    //Mover al personaje de el metodo NavMeshAgent
+    void MoverNPC()
+    {
+        float _distanciaActual = Vector3.Distance(transform.position, targetAgent.position);
+        distanciaActual = _distanciaActual;
+
+        if (_distanciaActual <= actionDistans)
+        {
+            agent.SetDestination(targetAgent.position);
+        }
+    }
+
+    //Detectar la velocidad y ejecuatar la animacion
+    void EjecutarAnimaciones()
+    {
         float velocidad = agent.velocity.magnitude;
 
 
